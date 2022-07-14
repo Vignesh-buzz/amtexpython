56ffd3a338e0205213e47f67ae202c5226dd84f9
Vignesh <104889172+Vignesh-buzz@users.noreply.github.com>
2022-07-14 15:07:04 +0530

    Add files via upload

--- loops&iteration.py
+++ loops&iteration.py
@@ -5,3 +5,12 @@ for num in x:
         print("found!")
         break
     print(num)
+#using continue function
+x = [11, 23, 45, 56, 3, 12, 89]
+for num in x:
+    if (num == 45):
+        print("found!")
+        continue
+    print(num)
+
+
