1e75c1a428f88f56dd2d26e85534e3a6c4227665
Vignesh <104889172+Vignesh-buzz@users.noreply.github.com>
2022-07-15 12:30:38 +0530

    Add files via upload

--- necty.py
+++ necty.py
@@ -1,4 +1,42 @@
-import my_module
+#from my_module import *
+#Here we are importing the python file
+"""import sys as system
+system.path.append('/Users/keerthu/PycharmProjects/pythonProject')
+#The above mention path is to fetch python file from another folder
+from my_module import find_index,test
 x=["math","science","social","english"]
-index=my_module.find_index(x,"social")
+index=find_index(x,"social")
 print(index)
+print(test)
+print(system.path)"""
+
+#to take the  random value in the python
+#import random inbuilt functios
+#it will give random string output
+"""import random
+courses=["Math","English","social","EVS","biology"]
+findcourse=random.choice(courses)
+print(findcourse)"""
+
+#to get the mathmetical operation
+"""import math
+x=math.sqrt(2)
+print(x)
+"""
+
+#to get the date & time
+#by using this we can get the date and time
+"""import datetime
+today=datetime.datetime.today()
+print(today)"""
+
+#to get the calender
+#we can use the import calender
+"""import calender
+print(calendar.isleap(2020))"""
+
+#to get the current woking directory
+"""import os
+print(os.getcwd())#to get the current working directory
+print(os.__file__)# this will give location of the file inside the file system.
+"""
