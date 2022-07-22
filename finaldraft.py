255fd9b4e8b6525489bb4956ea0911dc1b78e627
Vignesh <104889172+Vignesh-buzz@users.noreply.github.com>
2022-07-22 05:09:28 -0700

    Create finaldraft.py

---
+++ finaldraft.py
@@ -0,0 +1,27 @@
+import pymongo
+from pymongo import MongoClient
+class mongo:
+#To make the connection
+    def get_mongodb_connection(
+            host: str,
+            port: Union[str, int],
+            user: str,
+            password: str,
+
+    ):
+        return pymongo.MongoClient(
+            host=host,
+            port=int(port),
+            username=user,
+            password=password,
+        )
+
+    x = get_mongodb_connection('localhost', 27017, 'pallavi', '', 'filedata')
+    print(x)
+#This is to create tha database function
+     def db(dbname,collectionname):
+         myclient = MongoClient("mongodb://localhost:27017")
+         dbname = myclient.get_database()
+         collectionname = db.get_collection()
+    db("amtexdemo","amtex")
+
