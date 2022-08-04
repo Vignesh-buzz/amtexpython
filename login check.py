eb7950afbce8d9d2f55980a2eee0f7f92d653372
Vignesh <104889172+Vignesh-buzz@users.noreply.github.com>
2022-08-04 03:51:30 -0700

    Create login check.py

---
+++ login check.py
@@ -0,0 +1,25 @@
+uname = []
+upass = []
+x = str(input("enter the username : "))
+uname.append(x)
+y = str(input("enter the Password : "))
+upass.append(y)
+print(uname)
+print(upass)
+print("Please enter the login credentials")
+a = str(input("enter the username : "))
+b = str(input("enter the Password : "))
+if(a == x and b == y):
+    print("** connected successful **")
+    print("** Hello *Guna*/*Melson* Welcome to the star world **")
+    def pypart():
+        n = int(input("Enter the value for star = "))
+        for i in range(0, n):
+            for j in range(0, i + 1):
+                print("* ", end="")
+            print("\r")
+    pypart()
+    print("** I think your Requirement is correct ok bye... **")
+else:
+    print("check the connection")
+    print("ponga thambii poiyu veliya parungaa..!")
