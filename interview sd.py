7a8fe954420fd5b60578cf35f9ceebf091d8bae7
Vignesh <104889172+Vignesh-buzz@users.noreply.github.com>
2022-08-26 03:42:54 -0700

    Create interview sd.py

---
+++ interview sd.py
@@ -0,0 +1,42 @@
+#prime number
+"""num=int(input("enter the number = "))
+if num > 1:
+    for i in range(2, num):
+        if (num % i) == 0:
+            print(num, "is not a prime number")
+            break
+    else:
+        print(num, "is a prime number")
+else:
+    print(num, "is not a prime number")
+"""
+
+# odd or even
+"""x=int(input("enter the number = "))
+if(x%2==0):
+    print("it is even number")
+else:
+    print("it is odd number")
+"""
+
+#fibonnicseries
+"""x=int(input("enter the number = "))
+a=0
+b=1
+if(x==1):
+    print(a)
+else:
+    print(a)
+    print(b)
+    for i in range(2,x):
+        c=a+b
+        a=b
+        b=c
+        print(c)"""
+
+#palindrome
+"""x=str(input("enter the number = "))
+if(x == x[::-1]):
+    print(x, "it is a palindrome")
+else:
+    print(x, "its not panlidrome")"""
